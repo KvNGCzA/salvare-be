b2a6315d6f842449a6d42b371657daaee2bf1899
Christopher Akanmu <akanmuoluwadamilare@yahoo.com>
2020-01-26 18:26:45 +0100

    lwayer can drop a case (#11)

--- server/routes/case.js
+++ server/routes/case.js
@@ -4,12 +4,14 @@ import middlewares from '../middlewares';
 
 const route = express.Router();
 const { TokenUtils } = middlewares;
-const { createCase, assignCase } = CaseController;
+const { createCase, assignCase, dropCase } = CaseController;
 
 const base = '/case';
 
 route.post(`${base}/create`, createCase);
 
-route.patch(`${base}/assigncase/:caseId`, TokenUtils.verifyToken, assignCase);
+route.post(`${base}/assigncase/:caseId`, TokenUtils.verifyToken, assignCase);
+
+route.post(`${base}/dropcase/:caseId`, TokenUtils.verifyToken, dropCase);
 
 export default route;
